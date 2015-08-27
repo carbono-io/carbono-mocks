35692c588e330aef7188e8ab4ba3c59c0bffe994
Aline Takechi <akt@cesar.org.br>
2015-08-27 11:52:33 -0300

    JRD-163: jsdoc for createContainer function

--- ide-development-container-manager/app/controllers/container.js
+++ ide-development-container-manager/app/controllers/container.js
@@ -7,36 +7,17 @@ var cjr = new CJR('1.0');
  * Mocks the creation of a container that will host a new Machine (in this
  * example, it returns Code Machine's mock).
  *
- * #path POST /ide-development-container-manager/container
+ * Assuming that carbono-mocks is running at localhost:3000, you can call this
+ * function with this curl command:
+ *     curl -X POST localhost:3000/ide-development-container-manager/container/
+ *     -d '{"apiVersion":"1.0", "id":"23123-123123123-12312", "data":{"id":
+ *     "1234", "items": [{"projectId": "1234"}]}}' --verbose -H "Content-Type:
+ *     application/json"
  *
- * @param projectId Identifies a project, which will be associated to a
- *        container. If this association was already done, this identifier will
- *        be used to retrieve the correct image of the corresponding container.
- *        (#location body.data.items[0])
- *        (#required yes)
- *        (#schema {"projectId": <string>})
+ * @param {Object} req - Express object representing a Request
+ * @param {Object} res - Express object representing a Response
  *
- * #response 200 Container was successfully created / retrieved.
- *           (@schema {
- *                      "id": <string container_id>,
- *                      "items": [{
- *                          "url": <string machines_url>
- *                       }]
- *                    })
- *
- *           400 Malformed request
- *           (@schema {
- *                      "code": 400,
- *                      "message": "projectId cannot be null"
- *                    })
- *
- * #usage Assuming that carbono-mocks is running at localhost:3000, you can test
- *        this function with this curl command:
- *        curl -X POST
- *        localhost:3000/ide-development-container-manager/container/
- *        -d '{"apiVersion":"1.0", "id":"23123-123123123-12312",
- *        "data":{"id": "1234", "items": [{"projectId": "1234"}]}}' --verbose
- *        -H "Content-Type: application/json"
+ * @function createContainer
  */
 module.exports.createContainer = function (req, res) {
     try {
