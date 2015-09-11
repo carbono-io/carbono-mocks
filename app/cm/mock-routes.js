d39c8c9b2256c7b824bdc83a6ae1512093dfb972
Giulian Luz <giulian@Giulians-MacBook-Pro-2.local>
2015-09-11 17:18:55 -0300

    [JRD-324] Ajustes nomes e rotas

--- app/cm/mock-routes.js
+++ app/cm/mock-routes.js
@@ -4,28 +4,30 @@ var CJM = require('carbono-json-messages');
 function createRoutes(io) {
     console.log('yyyy');
 
-    io.on('connection', function (socket) {
-        console.log('has a client');
-        var profile = new CJM({id: 'x1', apiVersion: '1.0.0'});
-        profile.setData(
-            {
-                id: 'y2',
-                items: [
-                    {
-                       component: 'crud-basic',
-                       route: 'crud007'
-                    }
-                ]
-            }
-        );
+    io.of('code-machine').addListener('connection',
+        function (socket) {
+            console.log('has a client');
+            var profile = new CJM({id: 'x1', apiVersion: '1.0.0'});
+            profile.setData(
+                {
+                    id: 'y2',
+                    items: [
+                        {
+                            component: 'crud-basic',
+                            route: 'crud007'
+                        }
+                    ]
+                }
+            );
 
-        socket.on('installComponent',
-            function (data) {
-                console.log('installComponent', data);
-                socket.emit('installMachine', profile.toObject());
-            }
-        );
-    });
+            socket.on('command:insertElement',
+                function (data) {
+                    console.log('inserting element, I need a machine', data);
+                    socket.emit('project:createMachine', profile.toObject());
+                }
+            );
+        }
+    );
 }
 
 module.exports.createRoutes = createRoutes;
