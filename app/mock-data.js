d259525b876fab129d39bd699f9d62af69d229a3
Vitor Casadei <vitor@casadei.io>
2015-10-05 09:26:49 -0300

    Bugfixes

--- app/mock-data.js
+++ app/mock-data.js
@@ -99,9 +99,11 @@ var mock = function (app) {
                 res.setHeader('content-type', 'application/json');
                 res.statusCode = 201;
                 res.json(buildResponse(false, 201, "", {
-                    name: profile.name,
-                    email: profile.email,
-                    code: 'sjdh3434hdsj',
+                    profile: {
+                        name: profile.name,
+                        email: profile.email,
+                        code: 'sjdh3434hdsj',
+                    },
                 }).toObject());
                 res.end();
             } else if (profile.name === 'John Connor 400') {
@@ -127,10 +129,11 @@ var mock = function (app) {
             res.setHeader('content-type', 'application/json');
                 res.statusCode = 200;
                 res.json(buildResponse(false, 200, "", {
-                    name: 'John Connor',
-                    email: 'connor.john@resitance.com',
-                    code: req.params.code,
-                    
+                    profile: {
+                        name: 'John Connor',
+                        email: 'connor.john@resitance.com',
+                        code: req.params.code,
+                    },
                 }).toObject());
                 res.end();
         } else if (req.params.code == 'user400') {
@@ -157,10 +160,11 @@ var mock = function (app) {
             res.statusCode = 200;
             res.setHeader('content-type', 'application/json');
             res.json(buildResponse(false, 200, "", {
+                profile: {
                     name: 'John Connor',
                     email: req.headers.crbemail,
                     code: 'askjdhsakj3343',
-                    
+                },
                 }).toObject());
             res.end();
         } else if (req.headers.crbemail === 'email@400.com') {
@@ -195,7 +199,6 @@ var mock = function (app) {
                     name: 'John Connor',
                     email: profile.email,
                     code: 'sjdh3434hdsj',
-                    
                 }).toObject());
                 res.end();
             } else if (profile.email === 'email@400.com') {
@@ -527,9 +530,11 @@ var mock = function (app) {
                 res.setHeader('content-type', 'application/json');
                 res.statusCode = 201;
                 res.json(buildResponse(false, 201, '', {
-                    name: profile.name,
-                    email: profile.email,
-                    code: 'sjdh3434hdsj',
+                    profile: {
+                        name: profile.name,
+                        email: profile.email,
+                        code: 'sjdh3434hdsj',
+                    },
                 }).toObject());
                 res.end();
             } else if (profile.name === 'John Connor 400') {
@@ -555,10 +560,11 @@ var mock = function (app) {
             res.setHeader('content-type', 'application/json');
                 res.statusCode = 200;
                 res.json(buildResponse(false, 200, '', {
-                    name: 'John Connor',
-                    email: 'connor.john@resitance.com',
-                    code: req.params.code,
-                    
+                    profile: {
+                        name: 'John Connor',
+                        email: 'connor.john@resitance.com',
+                        code: req.params.code,
+                    }
                 }).toObject());
                 res.end();
         } else if (req.params.code == 'user400') {
@@ -585,9 +591,11 @@ var mock = function (app) {
             res.statusCode = 200;
             res.setHeader('content-type', 'application/json');
             res.json(buildResponse(false, 200, '', {
+                pofile: {
                     name: 'John Connor',
                     email: 'connor.john@resistance.com',
                     code: 'askjdhsakj3343',
+                },
                 }).toObject());
             res.end();
         } else if (req.headers.authorization === 'auth400') {
@@ -787,6 +795,7 @@ var mock = function (app) {
     // Updates a Project
     app.put('/imperial/projects/:code', function (req, res, next) {
         res.setHeader('content-type', 'application/json');
+        var projectData = req.body.data.items[0];
         if (req.headers.authorization === 'auth201') {
             res.statusCode = 201;
             res.json(buildResponse(false, 201, '', {
